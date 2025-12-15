05c8b0c3129a8e9676e4e975ac75f22efeef68ff
HauPhan-devJava2103 <phuchau.nam2005@gmail.com>
2025-12-15 16:11:57 +0700

    Config: Thymeleaf

---
+++ src/main/java/vn/web/fashionshop/config/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package vn.web.fashionshop.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(@NonNull ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/");
+        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
+        registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/static/assets/");
+
+        // Serve uploaded images từ thư mục uploads/images
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("file:uploads/images/");
+    }
+}
